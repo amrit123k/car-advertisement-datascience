d2258a0b2a6349b34e7fd2e0b129559670acbea8
amrit123k <amritkumar2020@gmail.com>
2025-04-25 15:38:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,6 @@ df = pd.read_csv('cleaned_data.csv')
 
 st.header('Vehicle Analysis')
 filtered_df = df[df['model_year'] >= 1990]
-df[['price', 'days_listed']] = df[['price', 'days_listed']].astype(float)
-filtered_df[['price', 'days_listed']] = filtered_df[['price', 'days_listed']].astype(float)
-# Fix unnamed columns
-df.columns = [f'col_{i}' if col is None else col for i, col in enumerate(df.columns)]
-filtered_df.columns = [f'col_{i}' if col is None else col for i, col in enumerate(filtered_df.columns)]
-
 if st.checkbox("Include Model Years before 1990"):
     st.dataframe(df)
 else:
