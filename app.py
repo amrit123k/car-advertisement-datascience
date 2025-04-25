2881387053346a011083d974881047048226988b
Amrit <amritkumar2020@gmail.com>
2025-04-25 13:39:34 -0400

    Add EDA notebook, Streamlit app, and cleaned data

---
+++ app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv('cleaned_data.csv')
+
+st.header('Vehicle Analysis')
+filtered_df = df[df['model_year'] >= 1990]
+if st.checkbox("Include Model Years before 1990"):
+    st.dataframe(df)
+else:
+    st.dataframe(filtered_df)
+
+fig = px.histogram(df, x='type', title='Distribution of Vehicle Types')
+st.plotly_chart(fig)
+
+fig = px.scatter(df, x='odometer', y='price', color='condition',
+                 title='Price vs Odometer by Condition',
+                 labels={'odometer': 'Mileage', 'price': 'Price ($)'})
+st.plotly_chart(fig)
+
+df_price_year = df.groupby('model_year')['price'].mean().reset_index()
+df_price_year = df_price_year[df_price_year['model_year']>=1974]
+df_price_year = df_price_year.sort_values(by = 'model_year')
+fig = px.line(df_price_year,
+              x='model_year',
+              y='price',
+              title='Price Inflation Over the Years')
+st.plotly_chart(fig)
+
+df_transmission = df.groupby('model_year',)['transmission'].value_counts().reset_index()
+df_transmission = df_transmission[df_transmission['model_year'] >= 1995]
+fig = px.bar(df_transmission,
+             x='model_year',
+             y='count',
+             color='transmission',
+             title='Transmission Type Counts by Model Year',
+             barmode='stack')
+st.plotly_chart(fig)
+
+
